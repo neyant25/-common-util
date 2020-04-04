e233de50ade9db0a41207fe473dd28b18d8ee32c
neyant25 <neyant@qq.com>
2020-04-04 21:18:19 +0800

    first commit

---
+++ sysadmin/organization/src/main/java/com/springboot/cloud/sysadmin/organization/entity/param/GroupQueryParam.java
@@ -0,0 +1,16 @@
+package com.springboot.cloud.sysadmin.organization.entity.param;
+
+import com.springboot.cloud.common.web.entity.param.BaseParam;
+import com.springboot.cloud.sysadmin.organization.entity.po.Group;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupQueryParam extends BaseParam<Group> {
+    private String name;
+}
